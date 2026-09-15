f70ef8dcf7b123e91edce8e9fc23ec506aea9842
Gary García <Gary.Garcia2703@gmail.com>
2026-09-15 17:06:55 -0300

    routes correction

--- front-end/src/api/GetAlbumDetails.js
+++ front-end/src/api/GetAlbumDetails.js
@@ -3,7 +3,7 @@ import API_URL from "./Config"
 
 async function GetAlbumDetails(id) {
     try {
-        const res = await axios.get(`${API_URL}/albums/${id}`)
+        const res = await axios.get(`${API_URL}albums/${id}`)
         return res.data
     } catch (error) {
         console.log(error)
--- front-end/src/api/GetAlbums.js
+++ front-end/src/api/GetAlbums.js
@@ -3,7 +3,7 @@ import API_URL from "./Config"
 
 async function GetAlbums() {
     try {
-        const res = await axios.get(`${API_URL}/albums`)
+        const res = await axios.get(`${API_URL}albums`)
         return res.data
     } catch (error) {
         console.log(error)
